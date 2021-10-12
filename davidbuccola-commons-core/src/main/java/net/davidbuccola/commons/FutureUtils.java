1c8d75c697cea983ff302981033c359238c8c5ed
David Buccola <dbuccola@salesforce.com>
2021-10-12 09:52:00 -0700

    Fix a piece of FutureUtils error reporting.

--- davidbuccola-commons-core/src/main/java/net/davidbuccola/commons/FutureUtils.java
+++ davidbuccola-commons-core/src/main/java/net/davidbuccola/commons/FutureUtils.java
@@ -1,23 +1,26 @@
 package net.davidbuccola.commons;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionException;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 
 import static java.util.Collections.synchronizedList;
 import static java.util.stream.Collectors.toList;
+import static net.davidbuccola.commons.Slf4jUtils.trace;
 
 /**
  * Utilities to help with {@link Future} and {@link CompletableFuture}.
  */
 public final class FutureUtils {
+
+    private static final Logger log = LoggerFactory.getLogger(FutureUtils.class);
 
     private FutureUtils() {
         throw new UnsupportedOperationException("Can't be instantiated");
@@ -45,32 +48,51 @@ public final class FutureUtils {
      * @param function        asynchronous operation applied to all elements
      */
     public static <A, R> CompletableFuture<List<R>> forAllOf(Collection<A> elements, boolean stopOnException, Function<A, CompletableFuture<R>> function) throws AggregateFutureException {
-        AtomicBoolean exceptionOccurred = new AtomicBoolean(false);
+        AtomicReference<Throwable> firstException = new AtomicReference<>();
         List<CompletableFuture<R>> elementFutures = synchronizedList(new ArrayList<>());
         for (A element : elements) {
-            if (!exceptionOccurred.get()) {
+            if (firstException.get() == null) {
+
+                trace(log, () -> "forAllOf: starting element processing: " + element);
+
                 CompletableFuture<R> elementFuture = function.apply(element);
                 elementFutures.add(elementFuture);
                 elementFuture.whenComplete((result, exception) -> {
-                    if (stopOnException && exception != null) {
-                        if (exceptionOccurred.compareAndSet(false, true)) {
-                            elementFutures.forEach(future -> future.cancel(true));
+
+                    trace(log, () -> "forAllOf: element processing finished: " + element + ": " + elementFuture);
+
+                    if (exception != null && !(exception instanceof CancellationException)) {
+                        if (firstException.compareAndSet(null, exception)) {
+                            if (stopOnException) {
+                                // Cancel remaining activity elements
+                                elementFutures.forEach(future -> {
+                                    if (!future.isDone()) {
+                                        future.cancel(true);
+                                    }
+                                });
+                            }
                         }
                     }
                 });
             } else {
+                trace(log, () -> "forAllOf: element cancelled before processing: " + element);
+
                 elementFutures.add(cancelledFuture());
             }
         }
 
         return CompletableFuture.allOf(toArray(elementFutures))
             .handle((nothing, exception) -> {
-                if (exception == null) {
-                    return elementFutures.stream().map(FutureUtils::awaitFutureResult).collect(toList()); // Aggregate element results
+                log.trace("forAllOf: all elements have completed (either successfully or exceptionally", exception);
+
+                if (firstException.get() != null) {
+                    throw new AggregateFutureException(elementFutures, firstException.get()); // Throw with element information attached
+
+                } else if (exception != null) {
+                    throw new AggregateFutureException(elementFutures, exception); // Throw with element information attached
 
                 } else {
-                    Throwable effectiveException = exception instanceof CompletionException ? exception.getCause() : exception;
-                    throw new AggregateFutureException(elementFutures, effectiveException); // Rethrow with element information
+                    return elementFutures.stream().map(FutureUtils::awaitFutureResult).collect(toList()); // Aggregate element results
                 }
             });
     }
@@ -131,7 +153,7 @@ public final class FutureUtils {
      */
     public static Throwable getExceptionFrom(CompletableFuture<?> future) {
         return getOptionalExceptionFrom(future)
-            .orElseThrow(() -> new AssertionError("Future did't fail"));
+            .orElseThrow(() -> new AssertionError("Future didn't fail"));
     }
 
     private static <U> CompletableFuture<U> cancelledFuture() {
@@ -140,7 +162,7 @@ public final class FutureUtils {
         return future;
     }
 
-    @SuppressWarnings({"unchecked", "SuspiciousToArrayCall"})
+    @SuppressWarnings({"unchecked"})
     private static <T> CompletableFuture<T>[] toArray(Collection<CompletableFuture<T>> elements) {
         return (CompletableFuture<T>[]) elements.toArray(new CompletableFuture[0]);
     }
